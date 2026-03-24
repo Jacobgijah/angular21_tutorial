2dfb9bbf17988184129a7eca3a97f498b6ca221a
jacobgijah <jacobgijjah@gmail.com>
2026-03-24 15:43:22 +0300

    bootstrap installation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DataBinding } from './components/data-binding/data-binding';
+import { Signal } from './components/signal/signal';
+import { Variables } from './components/variables/variables';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'databinding',
+        component: DataBinding
+    },
+    {
+        path: 'signal',
+        component: Signal
+    },
+    {
+        path: 'variables',
+        component: Variables
+    }
+];
