22fb977c3c0c0a55160a10bb121dcd42f4ac5d53
jacobgijah <jacobgijjah@gmail.com>
2026-03-25 11:40:27 +0300

    signals created

--- src/app/components/signal/signal.ts
+++ src/app/components/signal/signal.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signal',
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './signal.html',
   styleUrl: './signal.css',
 })
-export class Signal {}
+export class Signal {
+  // courseName: string = 'Angular Signals';
+
+  courseName = signal("Angular Signals");
+  student = signal({
+    name: 'John Doe',
+    age: 25,
+    course: 'Angular Signals'
+  });
+
+  constructor() {
+    console.log('Initial courseName:' + this.courseName());
+    setTimeout(() => {
+      this.courseName.set('Angular Signals Updated');
+      console.log('courseName updated to:' + this.courseName());
+    }, 5000);
+  }
+}
