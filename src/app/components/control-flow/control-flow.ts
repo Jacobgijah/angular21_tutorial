056b906a3c333aa392659a97f403dc45f828e722
jacobgijah <jacobgijjah@gmail.com>
2026-03-26 15:31:36 +0300

    flow-control is completed

--- src/app/components/control-flow/control-flow.ts
+++ src/app/components/control-flow/control-flow.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal, Signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { isActive } from '@angular/router';
 
 @Component({
   selector: 'app-control-flow',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './control-flow.html',
   styleUrl: './control-flow.css',
 })
-export class ControlFlow {}
+export class ControlFlow {
+
+  isOfferCodeAvl: boolean = false;
+  studentTotalMarks: number = 0;
+
+  isSuccesDivVisible: WritableSignal<boolean> = signal(false);
+
+  offerList: string[] = [
+    "20% off BtcPay",
+    "10% off MavaPay",
+    "15% off ChapPay",
+  ];
+
+  productCategoryList: string[] = [
+    "Electronics",
+    "Clothing",
+    "Books",
+    "Home & Kitchen",
+  ];
+
+  employeeList = [
+    { name: 'Alice', department: 'HR', isActive: true },
+    { name: 'Bob', department: 'Engineering', isActive: false },
+    { name: 'Charlie', department: 'Marketing', isActive: true },
+  ];
+
+  toggleDivVisibility() {
+    this.isSuccesDivVisible.set(!this.isSuccesDivVisible());
+  }
+}
